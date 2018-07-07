d6109054d92c3b21b935f6f5802fb3f2f92ec103
Rubén <rmunoz@iesfuentesanluis.org>
2018-07-07 10:56:52 +0200

    a

--- src/components/Principal.js
+++ src/components/Principal.js
@@ -18,14 +18,41 @@ class Principal extends Component {
         <div className='infoInicial'> A Contracorrent Esport Adaptat somos una asociación sin ánimo de lucro, que nace con el objetivo de promover la práctica del deporte, el desarrollo personal, la calidad de vida e inclusión social de personas con diversidad funcional o con riesgo de tenerla, a través de incentivary fomentar la práctica deportiva de una forma adaptada al deportista, y aprovechar los beneficios de la actividad física en la rehabilitación de niños con daño cerebral y otros problemas neurológicos.</div>
         <div className='zonaVista'>
 
-          <div className='marcoVision'>
+          <div className='marcoVision' ontouchstart="this.classList.toggle('hover');" >
             <div className='vision'>
-              <div className='misionTitulo'>Visión!</div>
-              <div className='misionText'> Llegar  al  máximo  número  de  personas  con  alguna  alteración  motora  pueda  disfrutar  de  la  práctica  deportiva.</div>
+              <div className='frontVision'>
+                <div className='misionTitulo'>Visión!</div>
+                <div className='misionText'> Llegar  al  máximo  número  de  personas  con  alguna  alteración  motora  pueda  disfrutar  de  la  práctica  deportiva.</div>
+              </div>
+              <div className='backVision'>
+              </div>
+            </div>
+          </div>
+          <div className='marcoVision' ontouchstart="this.classList.toggle('hover');" >
+            <div className='vision'>
+              <div className='frontValores'>
+                <div className='misionTitulo'>Valores!</div>
+                <div className='misionText'>Los  valores  que  nos  caracterizan  son:  la  inclusión  en  el  deporte,  trabajo  en  equipo,  responsabilidad y humildad.</div>
+              </div>
+              <div className='backValores'>
+                <div className='resaltar'>A Contracorrent Esport Adaptat</div>
+              </div>
+            </div>
+          </div>
+          <div className='marcoVision' ontouchstart="this.classList.toggle('hover');" >
+            <div className='vision'>
+              <div className='frontMision'>
+                <div className='misionTitulo'>MISIÓN!</div>
+                <div className='misionText'>Promover  la  práctica  del  deporte  y  realizar  actividades  deportivas  adaptadas  a  las  capacidades  de  los  participantes.</div>
+              </div>
+              <div className='backMision'>
+              </div>
             </div>
           </div>
 
-        <div className='marcoMision'>
+
+
+        {/* <div className='marcoMision'>
             <div className='mision'>
               <div className='misionTitulo'>MISIÓN!</div>
               <div className='misionText'>Promover  la  práctica  del  deporte  y  realizar  actividades  deportivas  adaptadas  a  las  capacidades  de  los  participantes.</div>
@@ -37,7 +64,7 @@ class Principal extends Component {
               <div className='misionTitulo'>Valores!</div>
               <div className='misionText'>  Los  valores  que  nos  caracterizan  son:  la  inclusión  en  el  deporte,  trabajo  en  equipo,  responsabilidad y humildad.</div>
             </div>
-        </div>
+        </div> */}
 
 
         </div>
